a080a328d464dafb7bb17b5a94d5b1bc8afe08c5
moonding <moondding@163.com>
2015-06-30 18:26:08 +0800

    测试函数

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -93,4 +93,8 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+    public function actionSay($message = 'Hello')
+    {
+        return $this->render('say', ['message' => $message]);
+    }
 }
